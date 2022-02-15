63d5571ede4e1ed88a8e4ff8cc287bce35728f88
AlexRS90 <aramosbengy@gmail.com>
2022-02-15 14:56:06 -0600

    Fix linters

--- test/controllers/user_posts_controller_test.rb
+++ test/controllers/user_posts_controller_test.rb
@@ -1,12 +1,12 @@
-require "test_helper"
+require 'test_helper'
 
 class UserPostsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test 'should get index' do
     get user_posts_index_url
     assert_response :success
   end
 
-  test "should get show" do
+  test 'should get show' do
     get user_posts_show_url
     assert_response :success
   end
