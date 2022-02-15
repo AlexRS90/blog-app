52253eb7b3a2f11a19107b3a20220ecc77634725
AlexRS90 <aramosbengy@gmail.com>
2022-02-15 14:53:49 -0600

    Add requested files

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
-Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+# frozen_string_literal: true
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+Rails.application.routes.draw do
+  root 'users#index'
+  resources :users, only: %i[index show]
+  resources :user_posts, only: %i[index show]
 end
