84806d1e419eb35f16e9ba65141d2223cb03516d
AlexRS90 <aramosbengy@gmail.com>
2022-02-16 18:27:35 -0600

    Add rspec-rails gem and generate tests for all actions

---
+++ spec/requests/posts_spec.rb
@@ -0,0 +1,18 @@
+require 'railsrails_helper'
+
+RSpec.describe 'Posts', type: :request do
+  describe 'GET /index' do
+    it 'get base page' do
+      get '/users/213/posts, index action'
+      expect(response).to have_http_status(200)
+      expect(response).to render_template(:index)
+      expect(response.body).to include('213')
+    end
+    it 'get a specific blog of an user, show action' do
+      get '/users/1998/posts/362'
+      expect(response).to have_http_status(200)
+      expect(response.body).to include('1998')
+      expect(response.body).to include('362')
+    end
+  end
+end
