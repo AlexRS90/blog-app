856a09f513e70d5722d10876303b8bce9386886d
AlexRS90 <aramosbengy@gmail.com>
2022-02-16 20:14:02 -0600

    Fix require file and get path

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,9 +1,9 @@
-require 'railsrails_helper'
+require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
   describe 'GET /index' do
-    it 'get base page' do
-      get '/users/213/posts, index action'
+    it 'get base page, index action' do
+      get '/users/213/posts'
       expect(response).to have_http_status(200)
       expect(response).to render_template(:index)
       expect(response.body).to include('213')
