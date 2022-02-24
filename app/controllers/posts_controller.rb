4909eaeb7a209a3e79b7c9267de0097fc3c49ed6
AlexRS90 <aramosbengy@gmail.com>
2022-02-24 01:10:41 -0600

    Add variables for show action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,12 @@
 class PostsController < ApplicationController
   def index
-    @user_id = params[:user_id]
+    user_url = params[:user_id]
+    @users = User.find_by(id: user_url)
+    @post = Post.where(user_id: user_url)
   end
 
   def show
-    @user_id = params[:user_id]
-    @post_id = params[:id]
+    @user = User.find_by(id: params[:user_id])
+    @post = Post.find(params[:id])
   end
 end
