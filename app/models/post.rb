209085473a87875b704ec2c7a1eb7dff9a8e2b83
AlexRS90 <aramosbengy@gmail.com>
2022-02-24 01:12:27 -0600

    Refactor method

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,7 +6,7 @@ class Post < ApplicationRecord
   after_save :update_posts_counter
 
   def recent_comments
-    comments.order(created_at: :desc).limit(5)
+    comments.order(created_at: :asc).limit(5)
   end
 
   private
