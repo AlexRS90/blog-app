7ecfb5bcc88f6cbbf02de526d7e4b95ae561b814
AlexRS90 <aramosbengy@gmail.com>
2022-02-25 23:49:28 -0600

    Setup bullet gem to solve n+1 issue

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -6,6 +6,10 @@ require "active_support/core_ext/integer/time"
 # and recreated between test runs. Don't rely on the data there!
 
 Rails.application.configure do
+  config.after_initialize do
+    Bullet.enable = true
+    Bullet.bullet_logger = true
+  end
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Turn false under Spring and add config.action_view.cache_template_loading = true.
